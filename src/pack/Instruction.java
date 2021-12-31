e853b13e42e2c17f066e3328a9fa412fa2afdce1
Hesham Moneer <hesham.moneer@outlook.com>
2021-12-31 18:35:47 +0200

    finished StringParser

--- src/pack/Instruction.java
+++ src/pack/Instruction.java
@@ -4,4 +4,14 @@ public class Instruction {
 	OpCode opCode;
 	int source1, source2, destination; //immediate?
 	
+	public Instruction(OpCode opCode, int source1, int source2, int destination) {
+		this.opCode = opCode;
+		this.source1 = source1;
+		this.source2 = source2;
+		this.destination = destination;
+	}
+	
+	public String toString() {
+		return opCode+" "+destination+" "+ source1 + " "+source2;
+	}
 }
